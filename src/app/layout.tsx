325e3e547136049c3c1a5cea44143944ef1e41d3
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-12 12:22:09 +0300

    0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portal Social NetWork",
-  description: "Social NetWork",
+  title: "Portal Crypto Social NetWork",
+  description: "Social network with integrated blockchain and AI agent",
 };
 
 export default function RootLayout({
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>test{children}</body>
     </html>
   );
 }
