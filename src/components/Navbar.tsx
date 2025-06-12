14d98f1257ee31b7b39b5816d4eb73426dfe5c47
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-12 14:27:37 +0300

    0.1.3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,58 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT */}
-      <div>
-        <Link href="/"
-          className="font-bold text-xl text-blue-600"
-        >PORTAL</Link>
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/"className="font-bold text-xl text-blue-600">
+          PORTAL
+        </Link>
       </div>
 
       {/* CENTER */}
-      <div className="hidden">
+      <div className="hidden md:flex text-sm w-[50%]">
+        {/* LINKS */}
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex gap-2">
+            <Image className="flex items-center gap-2"
+              src="/home.png"
+              alt="Home"
+              width={24}
+              height={16}
+            />
 
+            <span>HomePage</span>
+          </Link>
+
+          <Link href="/" className="flex gap-2">
+            <Image className="flex items-center gap-2"
+              src="/friends.png"
+              alt="Friends Page"
+              width={24}
+              height={16}
+            />
+
+            <span>Friends</span>
+          </Link>
+
+          <Link href="/" className="flex gap-2">
+            <Image className="flex items-center gap-2"
+              src="/stories.png"
+              alt="Stories Page"
+              width={24}
+              height={16}
+            />
+
+            <span>Stories</span>
+          </Link>
+        </div>
       </div>
 
       {/* RIGHT */}
-      <div>
+      <div className="flex items-center gap-4 xl:gap-8 justify-end w-[30%]">
         <MobileMenu />
       </div>
     </div>
