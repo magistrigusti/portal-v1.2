72522880761c6cd3bd6f3e2c64ab0c5744476c1d
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-15 17:52:38 +0300

    0.3.6a

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,32 @@
+// src/lib/mongodb.ts
+
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
-
-let client: MongoClient | null = null;
-
-export async function connectToDB() {
-  if (!client) {
-    client = new MongoClient(uri);
-    await client.connect();
-    console.log("✅ MongoDB подключен!");
-  }
-  return client.db(); // Можно добавить имя базы, если не в URI
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error("MONGODB_URI is not defined in environment variables");
 }
+
+  
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+
+declare global {
+ 
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
